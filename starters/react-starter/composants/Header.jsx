a6a3d6b98500e3b0cb94c2d654dffca549500ba4
hobyravaonatolotra <hobyravaonatolotra@gmail.com>
2026-04-23 05:42:11 +0300

    recuperation de Header.jsx dans le groupe1

---
+++ starters/react-starter/composants/Header.jsx
@@ -0,0 +1,12 @@
+// Header.jsx — GROUPE 1
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">💡 SmartIdea</h1>
+      <p className="header-slogan">Génère des idées grâce à l'intelligence artificielle</p>
+    </header>
+  );
+}
+
+export default Header;
